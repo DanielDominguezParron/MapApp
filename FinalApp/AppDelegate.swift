f27fd49becdd3bf798ea26a127eed9eae7481b2f
Dani <danixntl@gmail.com>
2019-12-08 07:42:07 -0800

    Core Data and UI improves
    Added CoreData Crud and stay logged user doesnt work yet

--- FinalApp/AppDelegate.swift
+++ FinalApp/AppDelegate.swift
@@ -7,7 +7,9 @@
 //
 
 import UIKit
-
+import Firebase
+import GoogleSignIn
+import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,8 +18,61 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        GIDSignIn.sharedInstance().clientID=FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance().delegate=self
+       // var viewController:ViewController=ViewController()
         return true
     }
+    @available(iOS 9.0, *)
+    func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any])
+        -> Bool {    return GIDSignIn.sharedInstance().handle(url)
+    }
+    //Google firebase signin
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        print("Running sign in")
+        if let error = error {
+            print("\(error.localizedDescription)")
+        } else {
+            let idToken = user.authentication.idToken
+        // User authentication
+        guard let authentication = user.authentication else { return }
+        //Store the google credential
+        let credential = GoogleAuthProvider.credential(withIDToken:idToken!, accessToken: authentication.accessToken)
+        
+        Auth.auth().signInAndRetrieveData(with: credential) { (authResult, error) in
+            if let error = error {
+                print("Firebase sign in error")
+                print(error)
+                return
+            }
+            //Segue to map screen
+            self.window?.rootViewController?.performSegue(withIdentifier: "segueMap", sender: self)
+            //Store the data from the current user into firestore
+            let uid=Auth.auth().currentUser?.uid
+            let db=Firestore.firestore()
+            db.collection("cities").document(uid!).setData([
+                "name": user.profile.name,
+                "mail": user.profile.email
+                ])
+            { err in
+                if let err = err {
+                    print("Error adding document: \(err)")
+                } else {
+                    print("Document successfully written!")
+                }
+            }
+            print("User is signed in with Firebase")
+        }
+        
+    }
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
+        // Perform any operations when the user disconnects from app here.
+        print("User has disconnected")
+    }
+
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -39,8 +94,47 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        // Saves changes in the application's managed object context before the application terminates.
+        self.saveContext()
     }
-
-
+    // MARK: - Core Data stack
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+         */
+        let container = NSPersistentContainer(name: "CoreDataCRUD")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    // MARK: - Core Data Saving support
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
 }
 
